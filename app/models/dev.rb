63bdbbb94f085d324714388bb55fbaaa5ec78f70
Lauren Erxleben <lauren.n.erxleben@gmail.com>
2022-07-27 17:26:12 -0600

    commit tree 4c62f6e9fee6152b1254360ed0214e31dcfad052

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
+has_many :freebies
+has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        freebies.any? {|f| f.item_name == item_name }
+    end
+
+    def give_away(freebie, dev)
+      freebie.dev == self ? freebie.update({dev: dev}) : nil
+    end
+
 
 end
